be66eec872a17bf975c6fac645e8c3be74494864
beshoy malak <beshoymalak190@gmail.com>
2023-09-11 16:32:53 -0700

    hi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Entry point*
+ *
+ * Description: A C  program that prints exactly Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (success)
+ */
 
-int main(void){
-    puts("\"Programming is like building a multilingual puzzle");
-    
+int main (void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
     return 0;
 }
