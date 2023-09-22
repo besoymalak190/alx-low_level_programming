a3b5a05cce4c99a1fd49debf44592abb6000719a
beshoy malak <beshoymalak190@gmail.com>
2023-09-22 15:59:04 -0700

    hi

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,22 +1,14 @@
 #include "main.h"
-#include <ctype.h>
 
 /**
- * _isupper - checks for uppercase character.
- *
- * @c: character will be checked.
- *
- * Return: 1 if uppercase characer and 0 if else.
-*/
-
+ * _isupper - checks if the character is uppercase
+ * @c:  is the int that will use for the argument of the function
+ * Return: 1 if uppercase character 0 if not
+ */
 int _isupper(int c)
 {
-	if (isupper(c))
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,16 +1,21 @@
-#include "main.h"
-
+#ifndef MAIN_H
+#define MAIN_H
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
 */
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 
-int main(void)
-{
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
-}
+#endif
