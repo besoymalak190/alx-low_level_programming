b9917fd1e7f62887c855acb79888500141fd2596
beshoy <beshoymalak1902gmail.com>
2023-11-01 18:59:23 -0700

    hi

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,83 +1,46 @@
+#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * strtow - Splits a string into words.
- * @str: The input string to be split.
+ * argstostr - concatenates all the arguments of your program.
  *
- * Return: A pointer to an array of strings (words).
- *         Each element of this array contains a single word, null-terminated.
- *         The last element of the returned array is NULL.
- *         Returns NULL if str is NULL or an empty string.
+ * @ac: size of array av.
+ * @av: array.
+ *
+ * Return: av.
  */
-char **strtow(char *str) {
-    if (str == NULL || str[0] == '\0') {
-        return NULL;
-    }
 
-    int word_count = 0;
-    int in_word = 0;
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' ')) {
-            word_count++;
-            in_word = 1;
-        } else if (str[i] == ' ') {
-            in_word = 0;
-        }
-    }
+char *argstostr(int ac, char **av)
+{
+	char *d;
+	int i, j, g = 0;
 
-    char **words = (char **)malloc((word_count + 1) * sizeof(char *));
-    if (words == NULL) {
-        return NULL;
-    }
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
 
-    int word_index = 0;
-    int word_start = -1;
-    in_word = 0;
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' ')) {
-            word_start = i;
-            in_word = 1;
-        } else if (str[i] == ' ') {
-            if (in_word) {
-                int word_length = i - word_start;
-                words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-                if (words[word_index] == NULL) {
-                    for (int j = 0; j < word_index; j++) {
-                        free(words[j]);
-                    }
-                    free(words);
-                    return NULL;
-                }
-                for (int j = 0; j < word_length; j++) {
-                    words[word_index][j] = str[word_start + j];
-                }
-                words[word_index][word_length] = '\0';
-                word_index++;
-                in_word = 0;
-            }
-        }
-    }
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; g++, j++)
+		{}
+		g++;
+	}
+	g++;
+	d = malloc(g * sizeof(char));
 
-    if (in_word) {
-        int word_length = strlen(str) - word_start;
-        words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-        if (words[word_index] == NULL) {
-            for (int j = 0; j <= word_index; j++) {
-                free(words[j]);
-            }
-            free(words);
-            return NULL;
-        }
-        for (int j = 0; j < word_length; j++) {
-            words[word_index][j] = str[word_start + j];
-        }
-        words[word_index][word_length] = '\0';
-        word_index++;
-    }
+	if (d == NULL)
+	{
+		return (NULL);
+	}
 
-    words[word_index] = NULL;
-
-    return words;
+	for (i = 0, g = 0; i < ac; i++, g++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, g++)
+		{
+			d[g] = av[i][j];
+		}
+		d[g] = '\n';
+	}
+			return (d);
 }
-
