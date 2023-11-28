b9773ca2b4b67c0ccb50ebeb93d2dc64c5e67d11
beshoy malak <beshoymalak190@gmail.com>
2023-11-28 02:59:19 -0800

    finish

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * create_file - Creates a file with specified content and permissions.
+ * @filename: The name of the file to create.
+ * @text_content: The content to write to the file.
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written;
+
+	fd = 0;
+	bytes_written = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[bytes_written] != '\0')
+			bytes_written++;
+
+		if (write(fd, text_content, bytes_written) != bytes_written)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
